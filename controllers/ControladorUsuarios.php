07f17fb3bac673e0b9dfc0791dc98d8ce89fb3c3
Olger Castro <olger60019castro@gmail.com>
2021-03-14 23:54:36 -0500

    Update

--- controllers/ControladorUsuarios.php
+++ controllers/ControladorUsuarios.php
@@ -29,12 +29,12 @@ class ControladorUsuarios
                                 icon: "success",
                                 title: "Acceso concedido...¡Bienvenido!",
                                 showConfirmButton: false,
-                                timer: 1500
+                                timer: 1300
                             });
                             function redirect(){
                                 window.location = "dashboard";
                             }
-                            setTimeout(redirect,1500);
+                            setTimeout(redirect,1300);
                              </script>';
                         } else {
                             echo '<script>
@@ -42,12 +42,12 @@ class ControladorUsuarios
                                 icon: "error",
                                 title: "Número de intentos de acceso excedidos, comuníquese con el administrador para desbloquear su cuenta",
                                 showConfirmButton: false,
-                                timer: 1800
+                                timer: 1400
                             });
                             function redirect(){
                                 window.location = "login";
                             }
-                            setTimeout(redirect,1800);
+                            setTimeout(redirect,1400);
                              </script>';
                         }
                         // Datos a usar en sesiones
@@ -58,12 +58,12 @@ class ControladorUsuarios
                             icon: "warning",
                             title: "Su cuenta de  usuario se encuentra desactivada, comuníquese con el administrador del sistema",
                             showConfirmButton: false,
-                            timer: 1600
+                            timer: 1400
                         });
                         function redirect(){
                             window.location = "login";
                         }
-                        setTimeout(redirect,1600);
+                        setTimeout(redirect,1400);
                             </script>';
                     }
                 } else if ($respuesta["cuenta"] == $_POST["logCuenta"]) {
@@ -84,12 +84,12 @@ class ControladorUsuarios
                         icon: "error",
                         title: "El usuario y/o contraseña ingresados no son correctos.' . $mensajeIntentos . '",
                         showConfirmButton: false,
-                        timer: 1800
+                        timer: 1400
                     });
                     function redirect(){
                         window.location = "login";
                     }
-                    setTimeout(redirect,2500);
+                    setTimeout(redirect,1400);
                 </script>';
                 } else {
                     echo '<script>
